4c1035d9d6dd00b7b405a43fe9d6f8465ac7c253
waterdog05 <84177301+waterdog05@users.noreply.github.com>
2021-12-31 18:22:54 +0900

    Update autoLight.ino

--- autoLight.ino
+++ autoLight.ino
@@ -1,22 +1,22 @@
 #include<Servo.h>
 #include <SoftwareSerial.h>
  
-SoftwareSerial mySerial(0, 1);  //시리얼 통신을 위한 객체선언
+SoftwareSerial mySerial(0, 1);  //tx, rx
 
 Servo myservo1;
 
 void setup()
 {
   myservo1.attach(12);
-  Serial.begin(9600);   //시리얼모니터
-  mySerial.begin(9600); //블루투스 시리얼
+  //Serial.begin(9600);
+  mySerial.begin(9600); //bluetooth
 }
 void loop()
 {
   char i;
   if (mySerial.available()) {
     i = mySerial.read();  
-    Serial.write(mySerial.read());  //블루투스측 내용을 시리얼모니터에 출력  
+    //Serial.write(mySerial.read());
   }
   if (i == '1'){
     myservo1.write(50);
